cd3401a5b5a5dd1325a6178c2e5ee41552341631
Ilnur Garrapov <garrapovilnur@gmail.com>
2022-12-16 18:22:57 +0300

    27th ex

--- Program.cs
+++ Program.cs
@@ -1,10 +1,8 @@
-﻿
+﻿/*
 int numberA = ReadInt("Введите число A: ");
 int numberB = ReadInt("Введите число B: ");
 ToDegree(numberA, numberB);
 
-
-// Функция возведения в степень
 void ToDegree(int a, int b)
 {
     int result = 1;
@@ -15,9 +13,41 @@ void ToDegree(int a, int b)
     Console.WriteLine(result);
 }
 
-// Функция ввода
 int ReadInt(string message)
 {
     Console.WriteLine(message);
     return Convert.ToInt32(Console.ReadLine());
+}
+*/
+
+int number = ReadInt("Введите число: ");
+int len = NumberLen(number);
+SumNumbers(number, len);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int NumberLen(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
 }
