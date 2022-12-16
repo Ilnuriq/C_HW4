d8ad15c283073c5d21656a4ad151fc12e91794d5
Ilnur Garrapov <garrapovilnur@gmail.com>
2022-12-16 18:31:42 +0300

    29th ex

--- Program.cs
+++ Program.cs
@@ -20,6 +20,7 @@ int ReadInt(string message)
 }
 */
 
+/*
 int number = ReadInt("Введите число: ");
 int len = NumberLen(number);
 SumNumbers(number, len);
@@ -50,4 +51,20 @@ void SumNumbers(int n, int len)
         n /= 10;
     }
     Console.WriteLine(sum);
+}
+*/
+
+int lenArray = ReadInt("Введите длинну массива: ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + " ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
 }
